d0d1626b028d284506c280efd3b2a39dff075ea5
Cristian Ciobanu <cciobanu@k1r2p12.42mulhouse.fr>
2022-07-14 16:05:36 +0200

    set

--- map.hpp
+++ map.hpp
@@ -267,11 +267,8 @@ namespace ft
             {
                 return tree.find(ft::make_pair(k, mapped_type()));
             }
-
-            size_type count (const key_type& k) const
-            {
-
-            }
+			
+		    size_type count (const key_type& k) const { return (find(k) != end() ? 1 : 0) }  //  TO-DO
 
             iterator lower_bound (const key_type& k)
             {

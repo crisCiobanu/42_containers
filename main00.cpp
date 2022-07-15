c14ea5e33ea5d335fff842f0434b577a627beb4c
Jeremy Perras <jeremy.perras@outlook.fr>
2022-07-15 08:45:00 +0200

    Stack

--- main00.cpp
+++ main00.cpp
@@ -2,9 +2,11 @@
 #include "map.hpp"
 #include "utils.hpp"
 #include "vector0.hpp"
+#include "stack.hpp"
 
 
-int main(void){
+int main(void)
+{
 
 	 ft::map< int,int > map;
 	 ft::vector< int> vector;
@@ -24,6 +26,50 @@ int main(void){
 		std::cout <<   *iter2  << std::endl;
 
 	std::cout << map.erase(5) <<std::endl;
+
+	ft::stack<int> mystack;
+
+	  for (int i=0; i<5; ++i) mystack.push(i);
+	  std::cout << "Popping out elements...";
+	  while (!mystack.empty())
+	  {
+	     std::cout << ' ' << mystack.top();
+	     mystack.pop();
+	  }
+	  std::cout << '\n';
+
+	// std::list<std::string>      std_lis;
+	// std::stack<char>            std_sta;
+    // std::stack<double>          std_que;
+    // std::map<int, std::string>  std_map;
+    // ft::vector<int>::iterator it;
+	//
+    // ft::vector<int>  std_vec(4,100);
+    // ft::vector<int> ft_vec(4,100);
+    // std::vector<int>::const_iterator it2;
+    // ft_vec.pop_back();
+    // it = ft_vec.begin();
+    // std::cout << *it <<std::endl;
+    // for (ft::vector<int>::const_iterator it2 = std_vec.begin(); it2 != std_vec.end(); ++it2)
+    // {
+	//
+    //         std::cout << ' ' << *it2 <<"\n";
+    //         std::cout << ' ' << *(it2 -  1)  <<"\n";
+	//
+    //           // *it2 = 200;
+    // }
+    // for (ft::vector<int>::iterator it = ft_vec.begin(); it != ft_vec.end(); ++it)
+    // {
+	//
+    //         std::cout << ' ' << *it <<"\n";
+    //         std::cout << ' ' << *(it - 1)  <<"\n";
+	//
+    //          *it = 200;
+    // }
+    // std::cout << '\n';
+
+
+    return(0);
 
 
 
